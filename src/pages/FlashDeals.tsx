49eb47fa52dd9f77e497d72b805b6a592efe3a0b
Leah Chan <164703268+lvchxeh@users.noreply.github.com>
2026-07-26 18:57:27 +0800

    finished flash deals page

--- src/pages/FlashDeals.tsx
+++ src/pages/FlashDeals.tsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react"
 import type { Product } from "../types"
 import { dummyProducts } from "../assets/assets"
 import { Zap } from "lucide-react"
+import Loading from "../components/Loading"
+import ProductCard from "../components/ProductCard"
 
 
 const FlashDeals = () => {
-  const [product, setProducts] = useState<Product[]>([])
+  const [products, setProducts] = useState<Product[]>([])
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
@@ -25,7 +27,25 @@ const FlashDeals = () => {
           </div>
           <p className="text-white/80 max-w-md mx-auto">Limited-time offers on your favorite organic products. Grab them before they're gone!</p>
         </div>
-        
+
+      </div>
+
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        {loading ? ( <Loading />) : (
+          products.length === 0 ? (
+            <div className="text-center py-16">
+              <Zap className="size-16 text-app-border mx-auto mb-4" />
+              <h2 className="text-lg font-semibold text-app-green mb-2">No deals right now</h2>
+              <p className="text-sm text-app-text-light">Check back soon for amazing offers!</p>
+            </div>
+          ) : (
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 gap-4">
+              {products.map((product) => product.stock > 0 && (
+                <ProductCard key={product._id} product={product}/>
+              ))}
+            </div>
+          )
+        )}
       </div>
 
     </div>
